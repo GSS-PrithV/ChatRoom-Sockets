38d06c28cfb8ae50917a3a1a5dcceca4bc44af37
Prithvinarayan Revuri <prevuri@stevens.edu>
2024-04-25 23:54:27 -0400

    Feat: Adding Groundwork for multiple chatrooms
    Changed route structure to support multiple rooms. Worked on  the client side JS and some server side JS for getting route information and room names.

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
+//Server file for the chat application
+
 import express from 'express';
 import {createServer} from 'http';
-import { fileURLToPath } from 'url';
-import { dirname,join } from 'path';
 import { Server } from 'socket.io';
+import exphbs from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const io = new Server(server, {connectionStateRecovery: {}});
 
 app.use('/public', express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//using handlebars, set the default view of the app at route /:id to index.html
+app.engine('html', exphbs.engine());
+app.set('view engine', 'html');
+
 //GET and POST requests to LH3000/
-app.get('/', (req,res) => {
-    res.sendFile(join(__dirname, 'views/index.html'));
+app.get('/:id', (req,res) => {
+    res.sendFile(path.resolve('views/index.html'));
 });
-app.post('/' , (req,res) => {
-    res.sendFile(join(__dirname, 'views/index.html'));
+app.post('/:id' , (req,res) => {
+    res.sendFile(path.resolve('views/index.html'));
 });
 
 //Socket connections 
 io.on('connection', (socket) => { //when a user connects to the server
-    console.log('a user connected');
+
+    socket.on('joinroom', (room) => { //when a user joins a room
+        socket.join(room);
+    });
+
+    io.emit('userConnection', 'A user has connected'); //sends a message to all users that a user has connected
 
     socket.on('disconnect', () => { //when a user disconnects from the server
-        console.log('user disconnected');
+        io.emit('userConnection', 'A user has disconnected');
     });
     
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', (msg) => { //when a user sends a message
+        io.emit('chat message', msg); //sends the message to all users
     });
 });
-
-
 
 server.listen(3000, () => {
     console.log("We've now got a server!");
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,11 @@
+//Client Side Script
+
 //Socket Connection
 socket = io();
 //
 
 //Elements
+roomName = document.getElementById('room-name');
 chat = document.getElementById('chat-form');
 input = document.getElementById('message');
 messageList = document.getElementById('message-list');
@@ -10,17 +13,26 @@ messageList = document.getElementById('message-list');
 
 
 if(chat){
+    roomName.innerHTML = 'Room: ' + window.location.pathname.split('/')[1];
     chat.addEventListener('submit', (e) => {
         e.preventDefault();
         if(input){
             console.log(input.value)
-            socket.emit('chat message', input.value);
+            socket.emit('chat message', input.value); //sends the message to the server
             input.value = '';
         }
     });
 }
 
-socket.on('chat message', (msg) => {
+socket.emit('joinroom',  console.log(window.location.pathname.split('/')[1])); //sends a message to all users that a user has connected
+
+socket.on('userConnection', (msg) => { //when the server sends the message to all users, recieve it here and append to the message list
+    let item = document.createElement('li');
+    item.innerHTML = msg;
+    messageList.appendChild(item);
+});
+
+socket.on('chat message', (msg) => { //when the server sends the message to all users, recieve it here and append to the message list
     let item = document.createElement('li');
     item.innerHTML = msg;
     messageList.appendChild(item);
